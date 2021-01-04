4257675f9b745b322e686c48e0e6d1832ba2ef98
JustinBonus <bonusj@uw.edu>
2021-01-04 13:04:10 -0800

    Exercise 1 problems 2 and 3, just finished first class session

--- code/c/ExerciseDay1/pi.c
+++ code/c/ExerciseDay1/pi.c
@@ -6,3 +6,25 @@
 // of F(xi)delta X, where F(xi) computed at middle of iterval i
 //
 // take as input the number of intervals N
+
+#include <stdio.h>
+#include <time.h>
+
+static int long numSteps = 1000;
+int main(int argc, char **argv){
+  double pi  = 0, time = 0;
+  clock_t tic, toc;
+  double dx, x;
+  dx = (double) 1/numSteps;
+
+  toc = clock();
+  for (int i=0; i<numSteps; i++){
+    x  = (double) i * dx;
+    pi += ( (4/(1+(x*x))) * dx);
+  }
+  tic = clock();
+  
+  time = ((double) (tic - toc)) / CLOCKS_PER_SEC;
+  printf("PI = %f, duration: %f ms\n", pi, time*1000);
+  return(0);
+}
--- code/c/ExerciseDay1/transformStress.c
+++ code/c/ExerciseDay1/transformStress.c
@@ -14,33 +14,31 @@
 #include <stdlib.h>
 #include <math.h>
 
-float transform(float sigmaX, float sigmaY, float tauXY, float theta);
+void transform(float X, float Y, float XY, float t);
 
-int main(int arc, char **argv){
+int main(int argc, char **argv){
 
   if (argc != 5) {
     printf("Usage: appName sigmaX sigmaY tauXY theta \n");
     exit(-1);
   }
 
-  float sigmaX = atof(argv[1]);
-  float sigmaY = atof(argv[2]);
-  float tauXY = atof(argv[3]);
-  float theta  = atof(argv[4]);
+  float sigmaX = atof(argv[1]);   // in kPa
+  float sigmaY = atof(argv[2]);   // in kPa
+  float tauXY  = atof(argv[3]);   // in kPa
+  float theta  = atof(argv[4]);   // in degrees
+  theta = theta * (M_PI / 180.0); // in radians
 
-
-  sigmaX, sigmaY, tauXY = transform(sigmaX, sigmaY, tauXY, theta);
-  printf("Transformed Stress: (%f, %f, %f) \n", sigmaX, sigmaY, tauXY, theta);
+  transform(sigmaX, sigmaY, tauXY, theta);
   return(0);
 }
 
 
-float transform(float sigmaX, float sigmaY, float tauXY, float theta){
-
+void transform(float sigmaX, float sigmaY, float tauXY, float theta){
   float sX, sY, tXY;
   sX = (sigmaX * (cos(theta)*cos(theta))) + (sigmaY * (sin(theta)*sin(theta))) + (2 * tauXY * sin(theta)*cos(theta));
   sY = (sigmaX * (sin(theta)*sin(theta))) + (sigmaY * (cos(theta)*cos(theta))) - (2 * tauXY * sin(theta)*cos(theta));
-  tXY = (sigmaX-sigmaY) * sin(theta) * cos(theta) + tauXY*(cos(theta)*cos(theta) - sin(theta)*sin(theta);
-  
-  return sX, sY, tXY;
+  tXY = (sigmaX-sigmaY) * sin(theta) * cos(theta) + tauXY*(cos(theta)*cos(theta) - sin(theta)*sin(theta));
+  printf("Transformed Stress (sigmaX', sigmaY', tauXY') [kPa]: (%f, %f, %f) \n", sX, sY, tXY);
+  return;
 }
