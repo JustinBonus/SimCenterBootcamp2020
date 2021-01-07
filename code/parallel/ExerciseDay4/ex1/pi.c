bbbaad4bae003da1011dc073050ac786d9208257
JustinBonus <bonusj@uw.edu>
2021-01-07 13:08:58 -0800

    Exercises for Day 4

--- code/parallel/ExerciseDay4/ex1/pi.c
+++ code/parallel/ExerciseDay4/ex1/pi.c
@@ -1,23 +1,68 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <mpi.h>
+#define LUMP 1
 
-static long int numSteps = 1000000000;
+static long int numSteps = 100000000;
 
-int main() {
+int main(int argc, char **argv) {
+
+  // Basic MPI initialization
+  int numP, procID;
+  printf("procID %d\n", procID);
+  
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &numP);
+  MPI_Comm_rank(MPI_COMM_WORLD, &procID);
+  printf("procID %d\n", procID);
+  
+  double *globalData=NULL;
+  double localData[LUMP];
+
+  // Set globalData to procID 0 heap array, initialize to zero
+  if (procID == 0){
+    globalData = (double *)malloc(LUMP * numP * sizeof(double));
+    for (int i=0; i<LUMP*numP; i++)
+      globalData[i]=0;
+  }
+
 
   // perform calculation
-  double pi   = 0;
-  double dx = 1./numSteps;
-  double x  = dx*0.50;
-  
-  for (int i=0; i<numSteps; i++) {
-    pi += 4./(1.+x*x);
-    x += dx;
+  double myPart = 0;
+  //double numSteps = LUMP*numP;
+  double dx   = 1./(numSteps);
+  double x = 0.50*dx;
+
+  for (int i=0; i<numSteps; i+=numP){
+    x = (i+0.5)*dx;
+    myPart += 4./(1.+x*x);
   }
   
-  pi *= dx;
+  myPart *= dx;
   
-  printf("PI = %16.14f Difference from math.h definition %16.14f \n",pi, pi-M_PI);
+  for (int i=0; i<LUMP; i++) {
+    localData[i] = myPart;
+  }
+  
+  MPI_Gather(localData, LUMP, MPI_DOUBLE, globalData, LUMP, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+
+  if (procID == 0) {
+    double pi = 0;
+    for (int i=0; i<numP*LUMP; i++){
+      pi += globalData[i];
+    }
+    printf("\n");
+    printf("PI = %16.14f Difference from math.h definition %16.14f \n",pi, pi-M_PI);
+  }
+  
+  if (procID == 0) {
+    free(globalData);
+  }
+
+  MPI_Finalize();
+
   return 0;
 }
--- code/parallel/ExerciseDay4/ex2/pi.c
+++ code/parallel/ExerciseDay4/ex2/pi.c
@@ -1,23 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <omp.h>
 
-static long int numSteps = 1000000000;
+static long int numSteps = 100000000;
 
-int main() {
-
-  // perform calculation
-  double pi   = 0;
-  double dx = 1./numSteps;
-  double x  = dx*0.50;
+int main(int argc, char **argv) {
+  int nThreads = 0;
+  double pi = 0;
+  double sum[64]; // Can reach 125336 threads Ubuntu 18.04, i7 12-core, 12 GB RAM
   
-  for (int i=0; i<numSteps; i++) {
-    pi += 4./(1.+x*x);
-    x += dx;
+#pragma omp parallel
+  {
+    // Process info
+    int id = omp_get_thread_num();
+    int numP = omp_get_num_threads();
+
+    // Set thread number on processor 0
+    if (id == 0) nThreads = numP;
+    
+    // Calculation Variables
+    double myPart = 0;
+    double dx = 1./numSteps;
+    double x = dx*0.50;
+
+
+    // Calculate pi-partial
+    for (int i=0; i<numSteps; i+=numP){
+      x = (i+0.5)*dx;
+      myPart += 4./(1.+x*x);
+    }
+
+    // Place part into array element
+    myPart *= dx;
+    sum[id] = myPart;
   }
-  
-  pi *= dx;
-  
+
+  // Sum (reduce) array elements into pi
+  for (int i=0; i<nThreads; i++)
+    pi+= sum[i];
+
+  // Print to screen
+  printf("Total Number of Threads: %d\n", nThreads);
   printf("PI = %16.14f Difference from math.h definition %16.14f \n",pi, pi-M_PI);
   return 0;
 }
